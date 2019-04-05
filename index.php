79327e9cc09990a1d0b4b6c041ba301160adb780
Max <maxime.brunet@live.fr>
2019-04-05 10:55:12 +0200

    Ajout de la page (index.php)

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Activité OpenClassRoom</title>
+	</head>
+
+	<body>
+		<h1>Ceci est une alert !</h1>
+		<script src="script.js"></script>
+	</body>	
+
+</html>
